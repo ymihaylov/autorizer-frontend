8e205782f9bd8e0d45b20c4ba95f3a2789269056
Yavor Mihaylov <yavor.st.m@gmail.com>
2021-10-05 00:29:58 +0300

    First commit

---
+++ garage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>autOrizer</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<?php require_once './header.php'?>
+
+<div id="main-container" class="container">
+
+</div>
+
+<div id="footer-container" class="container">
+    <footer class="text-center">
+        © 2020 Copyright:
+        <a class="" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </footer>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,38 @@
+<header class="p-3 mb-3 border-bottom">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a id="logo-link" href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+                <img src="img/logo.png" />
+            </a>
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2 link-secondary">Моят гараж</a></li>
+                <li><a href="#" class="nav-link px-2 link-dark">Сервизи</a></li>
+                <li><a href="#" class="nav-link px-2 link-dark">Оферти</a></li>
+                <li><a href="#" class="nav-link px-2 link-dark">Напомняния</a></li>
+            </ul>
+
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                <input type="search" class="form-control" placeholder="Търсене ..." aria-label="Search">
+            </form>
+
+            <!--            <div class="text-end">-->
+            <!--                <button type="button" class="btn btn-outline-primary me-2">Влез</button>-->
+            <!--                <button type="button" class="btn btn-primary">Регистрация</button>-->
+            <!--            </div>-->
+
+            <div class="dropdown text-end">
+                <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="img/empty-profile-picture.png" alt="mdo" width="32" height="32">
+                    <!--                    <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">-->
+                </a>
+                <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1" style="">
+                    <li><a class="dropdown-item" href="#">Лични настройки</a></li>
+                    <li><a class="dropdown-item" href="#">Настройки на изгледа</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Изход</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
